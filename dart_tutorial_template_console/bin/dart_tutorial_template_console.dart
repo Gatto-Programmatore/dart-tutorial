48ffefef323081ec2b2caa3205d8688ceeab91c4
Gatto Programmatore <gatto.programmatore@gmail.com>
2025-06-11 11:12:35 +0200

    aggiunta cartella progetto dart installato da win powershell

---
+++ dart_tutorial_template_console/bin/dart_tutorial_template_console.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tutorial_template_console/dart_tutorial_template_console.dart' as dart_tutorial_template_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tutorial_template_console.calculate()}!');
+}
---
+++ dart_tutorial_template_console/test/dart_tutorial_template_console_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tutorial_template_console/dart_tutorial_template_console.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
